fc601f213318121d45033c8564b5b2521eb4fc1b
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 02:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var empArray = [];
   for (let i = 0; i < musicians.length; i++) {
-    empArray.push(`"${musicians[i]} plays ${instruments[i]}"`)
+    empArray.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return empArray
 }
